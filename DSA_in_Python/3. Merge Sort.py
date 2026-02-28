837cf20ddf5efe371756468867d0ca924c6db9d6
Sainathan V <sainathan.sssihl@gmail.com>
2026-02-28 15:34:44 +0530

    Simple Merge Sort

---
+++ DSA_in_Python/3. Merge Sort.py
@@ -0,0 +1,42 @@
+# Today let's write a merge sort algorithm in python
+
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2  # Finding the mid of the array
+        L = arr[:mid]        # Dividing the array elements into 2 halves
+        R = arr[mid:]
+
+        merge_sort(L)        # Sorting the first half
+        merge_sort(R)        # Sorting the second half
+
+        i = j = k = 0
+        
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        # Checking if any element was left
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+    
+    return arr
+
+# Let us try this with an example!
+if __name__ == "__main__":
+    arr = [38, 27, 43, 3, 9, 82, 10]
+    print("Given array is:", arr)
+    sorted_arr = merge_sort(arr)
+    print("Sorted array is:", sorted_arr)
