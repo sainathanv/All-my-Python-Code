50e5d96c2ecaae35705635cfefce76bdd415247e
Sainathan V <sainathan.sssihl@gmail.com>
2026-03-02 15:45:07 +0530

    Something spiritual and fun!

---
+++ .vscode/12. Likhita Nama Japam.py
@@ -0,0 +1,11 @@
+#Likhita Japam
+s="Aum Sri Sairam"
+i=1
+n=int(input("Enter the number of times: "))
+while(i<=n+1):
+    print(s,end='\n')
+    i=i+1
+print("\n")
+print("\nJai Sai Ram")
+print("\n")
+print("\n")
