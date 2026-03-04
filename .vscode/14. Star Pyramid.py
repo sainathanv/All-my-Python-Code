f44e82ab231b2b406ecb3ff0ec6ceb63c33ffe33
Sainathan V <sainathan.sssihl@gmail.com>
2026-03-04 13:55:01 +0530

    Fun with coding!

---
+++ .vscode/14. Star Pyramid.py
@@ -0,0 +1,15 @@
+'''
+
+*
+* *
+* * *
+* * * *
+* * * * *
+Let's try this using for loops.
+'''
+
+
+for i in range(1,6): # 1 2 3 4 5
+    for j in range(i):
+        print('*', end=' ')
+    print()
