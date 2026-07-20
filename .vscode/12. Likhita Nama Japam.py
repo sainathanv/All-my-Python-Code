de3b068835a049252218c7ea2886887f7b77a1eb
Sainathan V <sainathan.sssihl@gmail.com>
2026-07-20 21:04:42 +0530

    updated_py_file_12

--- .vscode/12. Likhita Nama Japam.py
+++ .vscode/12. Likhita Nama Japam.py
@@ -1,11 +1,19 @@
 #Likhita Japam
-s="Aum Sri Sairam"
-i=1
-n=int(input("Enter the number of times: "))
-while(i<=n+1):
-    print(s,end='\n')
-    i=i+1
-print("\n")
-print("\nJai Sai Ram")
-print("\n")
-print("\n")
+def likhita_nama_japam(num):
+    s="Aum Sri Sairam"
+    i=1
+    while(i<=num+1):
+        print(s,end='\n')
+        i=i+1
+    
+    print("\n")
+    print("\nJai Sai Ram")
+    print("\n")
+    print("\n")
+
+try:
+    n = int(input("Enter the number of times = "))
+    likhita_nama_japam(n)
+
+except ValueError:
+    print("Error! Enter valid number")
